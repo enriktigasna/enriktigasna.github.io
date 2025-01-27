fef425a308f0dc694607799ca50c673eb7793bd3
enriktigasna <maxim.pavlenko2011@gmail.com>
2025-01-27 18:17:15 +0100

    Prerender

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,6 +1,7 @@
 import type { PostMetadata } from '$lib/types';
 import type { PageLoad } from './$types';
 
+export const prerender = true;
 export const load: PageLoad = async (): Promise<{ posts: PostMetadata[] }> => {
 	const postFiles = import.meta.glob<{metadata: PostMetadata}>('../posts/*.md', { eager: true });
 
--- src/routes/posts/[slug]/+page.ts
+++ src/routes/posts/[slug]/+page.ts
@@ -2,6 +2,7 @@ import type { Post } from '$lib/types';
 import type { PageLoad } from './$types';
 import { error } from '@sveltejs/kit';
 
+export const prerender = true;
 export const load: PageLoad = async ({ params }): Promise<Post> => {
 	try {
 		const file = await import(`../../../posts/${params.slug}.md`);
